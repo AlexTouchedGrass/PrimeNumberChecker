6619f8c6d423cd789094168f057472e06a24dbed
AlexTouchedGrass <alexdangerdean@gmail.com>
2023-11-13 21:13:26 -0500

    StackOverflow and ChatGPT are the gods that govern our world. That is all.

--- PrimeNumberChecker.java
+++ PrimeNumberChecker.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class PrimeNumberChecker {
+import java.util.Scanner;
+
+public class PrimeNumberChecker {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int num;
+        boolean valid = true;
+
+        System.out.println("Please enter a number.");
+        num = scan.nextInt();
+
+        if (num == 0 || num == 1) {
+            System.out.println("These numbers are not considered prime numbers in this program.");
+        } else {
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    valid = false;
+                    break; // Breaks out of the loop if the number is not prime, which makes my job so easy. (YES I GOT THIS FROM ONLINE, YES I KNOW WE HAVEN'T LEARNED THIS YET
+                }
+            }
+        }
+
+        System.out.println(valid);
+    }
 }
